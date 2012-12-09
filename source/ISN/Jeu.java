3faac4e324e02d0a55bcee49a25857efb5d830a5
victorlevasseur <victorlevasseur01@orange.fr>
2012-12-09 16:02:37 +0100

    Ajout du code de base pour le lancement de la fenêtre

--- source/ISN/Jeu.java
+++ source/ISN/Jeu.java
@@ -1,10 +1,44 @@
 package ISN;
 
+import org.jsfml.graphics.*;
+import org.jsfml.system.Clock;
+import org.jsfml.system.Time;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
 public class Jeu {
 
 	public static void main(String[] args) {
 		
+		RenderWindow fenetre = new RenderWindow();
+		fenetre.create(new VideoMode(1024, 768, 32), "Jeu ISN");
 		
+		Clock clock = new Clock();
+		
+		while(fenetre.isOpen()){
+			
+			//Boucle pour récupérer chaque événement (clic de souris, touche clavier) reçu par la fenêtre
+			for(Event event : fenetre.pollEvents()){
+				
+				//event représente l'événement qui sera traité.
+				//event.type donne le type d'événement (clic de souris, clavier...)
+				//Selon le type de l'événement, il faut utiliser event.asBLABLA() pour récupérer les
+				//informations relatives à l'événement (touche du clavier, bouton de souris, position du clic).
+				
+				if(event.type == Event.Type.CLOSED) {
+		            //L'utilisateur a décidé de fermer le jeu.
+		            fenetre.close();
+		            
+		        }
+			}
+			
+			fenetre.clear();
+			
+			//Boucle infinie pour gérer le jeu
+			Time dureeFrame = clock.restart();
+			
+			fenetre.display();
+		}
 	}
 
 }
