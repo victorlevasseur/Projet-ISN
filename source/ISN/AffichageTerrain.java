03b42f6c96892510b34abddc0eb13a1e7ce5aae5
victorlevasseur <victorlevasseur01@orange.fr>
2013-02-28 15:25:20 +0100

    Correction sur le rectangle.

--- source/ISN/AffichageTerrain.java
+++ source/ISN/AffichageTerrain.java
@@ -1,9 +1,13 @@
 package ISN;
+
 import org.jsfml.graphics.RectangleShape;
+import org.jsfml.system.Vector2f;
+import org.jsfml.graphics.Color;
 
 public class AffichageTerrain {
 	public static void afficherterrain(){
-	RectangleShape monRectangle = new RectangleShape();
-	rectangle.setSize(newVector2f(1024, 1));
+		RectangleShape monRectangle = new RectangleShape();
+		monRectangle.setSize(new Vector2f(1024, 1));
+		monRectangle.setFillColor(new Color(255, 255, 255, 255));
 	}
 }
