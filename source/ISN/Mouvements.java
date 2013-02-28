97cb23c628b3e7a08740cb205844038788d4d439
pleyo95 <2804pierre@neuf.fr>
2013-02-28 16:18:50 +0100

    deplacements

--- source/ISN/Mouvements.java
+++ source/ISN/Mouvements.java
@@ -1,5 +1,14 @@
 package ISN;
+import org.jsfml.system.*;
+import org.jsfml.window.Keyboard;
 
 public class Mouvements {
-
+	public static void deplacements(float dureeFrame){
+		Vector2f positionJ1 = Jeu.personnages[0].getPosition();
+		if (positionJ1.x - Jeu.rPerso>0 && Keyboard.isKeyPressed(Keyboard.Key.Q)) {
+			
+			
+		}
+			
+	}
 }
