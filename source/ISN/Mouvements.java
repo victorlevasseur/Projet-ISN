8d5cbb14a7636231b55b792f76a131c076860b80
victorlevasseur <victorlevasseur01@orange.fr>
2013-02-28 16:37:21 +0100

    Corrections sur le mouvement Changement de vitesse de déplacement

--- source/ISN/Mouvements.java
+++ source/ISN/Mouvements.java
@@ -5,10 +5,11 @@ import org.jsfml.window.Keyboard;
 public class Mouvements {
 	public static void deplacements(float dureeFrame){
 		Vector2f positionJ1 = Jeu.personnages[0].getPosition();
-		if (positionJ1.x - Jeu.rPerso>0 && Keyboard.isKeyPressed(Keyboard.Key.Q)) {
-			positionJ1.x = positionJ1.x - 40*dureeFrame;
-			Jeu.personnages[0].setPosition(positionJ1);
+		if (positionJ1.x - Jeu.rPerso>0 && Keyboard.isKeyPressed(Keyboard.Key.Q)) {		
 			
+			//move permet de bouger l'objet par rapport à sa position actuelle.
+			//Une vitesse de 150 px/s est idéale ! :)
+			Jeu.personnages[0].move(-140*dureeFrame, 0);
 		}
 			
 	}
