ba618d3aa81f0466635372884aa9f839c52b9ba0
victorlevasseur <victorlevasseur01@orange.fr>
2013-02-28 17:04:54 +0100

    Ajout d'un début de physique à la balle ! :D

--- source/ISN/Jeu.java
+++ source/ISN/Jeu.java
@@ -22,15 +22,15 @@ public class Jeu {
 	public static Sprite balle;
 	public static float rBalle;
 	
-	public static Sprite filet;
-	
+	//Pour le moteur physique
 	public static float vitesseX;
 	public static float vitesseY;
-	
 	public static float g;
+	public static boolean[] dejaCollision;
 	
 	public static Texture texturePerso1;
 	public static Texture texturePerso2;
+	public static Texture textureBalle;
 
 	public static void main(String[] args) {
 		
@@ -73,27 +73,34 @@ public class Jeu {
 	
 	public static void initialisation()
 	{
+		rPerso = 40;
+		rBalle = 7;
+		
+		g = 400;
+		vitesseX = 0;
+		vitesseY = 0;
+		dejaCollision = new boolean[2];
+		dejaCollision[0] = false;
+		dejaCollision[1] = false;
+		
 		personnages = new Sprite[2];
 		personnages[0] = new Sprite();
 		personnages[1] = new Sprite();
 		
-		personnages[0].setOrigin(40, 40);
-		personnages[1].setOrigin(40, 40);
+		personnages[0].setOrigin(rPerso, rPerso);
+		personnages[1].setOrigin(rPerso, rPerso);
 		
 		personnages[0].setPosition(256, 763);
 		personnages[1].setPosition(256 + 512, 763);
 		
-		filet = new Sprite();
 		balle = new Sprite();
 		
-		rPerso = 40;
-		rBalle = 7;
-		
-		vitesseX = 0;
-		vitesseY = 0;
+		balle.setOrigin(rBalle, rBalle);
+		balle.setPosition(256, 200);
 		
 		texturePerso1 = new Texture();
 		texturePerso2 = new Texture();
+		textureBalle = new Texture();
 		
 		try {
 			texturePerso1.loadFromFile(new File("IMAGE/persoJ1.png")); 
@@ -107,12 +114,20 @@ public class Jeu {
 			
 		}
 		
+		try {
+			textureBalle.loadFromFile(new File("IMAGE/balle.png")); 
+		} catch(IOException ex) { 
+			
+		}
+		
 		personnages[0].setTexture(texturePerso1);
 		personnages[1].setTexture(texturePerso2);
+		balle.setTexture(textureBalle);
 	}
 	
 	public static void gererJeu(float dureeFrame){
 		Mouvements.deplacements(dureeFrame);
+		Physique.gererPhysique(dureeFrame);
 		//Ici, on gérera le jeu (mouvements des persos, menu, collisions...)
 		
 	}
@@ -123,6 +138,7 @@ public class Jeu {
 		
 		fenetre.draw(personnages[0]);
 		fenetre.draw(personnages[1]);
+		fenetre.draw(balle);
 	}
 
 }
--- source/ISN/Physique.java
+++ source/ISN/Physique.java
@@ -2,10 +2,34 @@ package ISN;
 
 import org.jsfml.system.*;
 import org.jsfml.graphics.*;
+import java.lang.*;
 
 public class Physique {
-	public static void gererPhysique()
+	public static void gererPhysique(float dureeFrame)
 	{
+		Vector2f positionBalle = Jeu.balle.getPosition();
 		
+		Jeu.vitesseY += Jeu.g * dureeFrame;
+		
+		for(int j = 0; j < 2; j++)
+		{
+			Vector2f positionPerso = Jeu.personnages[j].getPosition();
+			double distanceCarre = Math.pow(positionBalle.x - positionPerso.x, 2) + Math.pow(positionBalle.y - positionPerso.y, 2);
+			
+			if(Math.sqrt(distanceCarre) < (Jeu.rPerso + Jeu.rBalle))
+			{
+				if(!Jeu.dejaCollision[j])
+				{
+					Jeu.vitesseY = -Jeu.vitesseY;
+				}
+				Jeu.dejaCollision[j] = true;
+			}
+			else
+			{
+				Jeu.dejaCollision[j] = false;
+			}
+		}
+		
+		Jeu.balle.move(Jeu.vitesseX * dureeFrame, Jeu.vitesseY * dureeFrame);
 	}
 }
