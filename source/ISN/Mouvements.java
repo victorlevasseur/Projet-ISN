61f0515c8aabe55cf6f790173030619ab22c8af6
pleyo95 <2804pierre@neuf.fr>
2013-02-28 16:30:49 +0100

    BORDEL DE MEEEEERDE !!

--- source/ISN/Mouvements.java
+++ source/ISN/Mouvements.java
@@ -6,7 +6,8 @@ public class Mouvements {
 	public static void deplacements(float dureeFrame){
 		Vector2f positionJ1 = Jeu.personnages[0].getPosition();
 		if (positionJ1.x - Jeu.rPerso>0 && Keyboard.isKeyPressed(Keyboard.Key.Q)) {
-			
+			positionJ1.x = positionJ1.x - 40*dureeFrame;
+			Jeu.personnages[0].setPosition(positionJ1);
 			
 		}
 			
