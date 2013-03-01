8c7279fe1fc48582eeae76476d086057b70d8b86
pleyo95 <2804pierre@neuf.fr>
2013-03-01 14:17:17 +0100

    mouvements terminés

--- source/ISN/Mouvements.java
+++ source/ISN/Mouvements.java
@@ -11,11 +11,12 @@ public class Mouvements {
 			//Une vitesse de 150 px/s est idéale ! :)
 			Jeu.personnages[0].move(-140*dureeFrame, 0);
 		}
-		if (positionJ1.x - Jeu.rPerso<507 && Keyboard.isKeyPressed(Keyboard.Key.D)) {		
+		if (positionJ1.x + Jeu.rPerso<507 && Keyboard.isKeyPressed(Keyboard.Key.D)) {		
 			
 			//move permet de bouger l'objet par rapport à sa position actuelle.
 			//Une vitesse de 150 px/s est idéale ! :)
 			Jeu.personnages[0].move(+140*dureeFrame, 0);
+		}
 		Vector2f positionJ2 = Jeu.personnages[1].getPosition();
 		if (positionJ2.x - Jeu.rPerso>517 && Keyboard.isKeyPressed(Keyboard.Key.LEFT)) {		
 			
@@ -23,12 +24,11 @@ public class Mouvements {
 			//Une vitesse de 150 px/s est idéale ! :)
 			Jeu.personnages[1].move(-140*dureeFrame, 0);
 		}
-		if (positionJ2.x - Jeu.rPerso<1024 && Keyboard.isKeyPressed(Keyboard.Key.RIGHT)) {		
+		if (positionJ2.x + Jeu.rPerso<1024 && Keyboard.isKeyPressed(Keyboard.Key.RIGHT)) {		
 			
 			//move permet de bouger l'objet par rapport à sa position actuelle.
 			//Une vitesse de 150 px/s est idéale ! :)
 			Jeu.personnages[1].move(+140*dureeFrame, 0);
-			}			
-		}
+		}			
 	}
 }
